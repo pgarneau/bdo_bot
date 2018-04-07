5a6333d1ccb3016fa35405dcbc6657c492ef462f
Philippe <philippegarneau1@gmail.com>
2018-04-07 13:42:22 -0400

    First commit

---
+++ bot/input/input.py
@@ -0,0 +1,26 @@
+import pyautogui
+
+from input import coordinates
+
+MOVE_SPEED = 0.5
+
+def init():
+    pyautogui.PAUSE = 2
+
+def move(x, y):
+    pyautogui.moveTo(x, y, 0.6)
+
+def testMove():
+    pyautogui.moveTo(coordinates.TEST[0], coordinates.TEST[1], MOVE_SPEED)
+
+def store():
+    # Move to 1st slot in inventory
+    # Right click
+    # Press f
+    # Press space
+
+    # Move to 2nd slot
+    # Right click
+    # f 
+    # space
+    print('placeholder')
---
+++ bot/main.py
@@ -0,0 +1,9 @@
+from input import input
+
+def main():
+    input.init()
+    input.testMove()
+    print("Hello")
+
+if __name__== "__main__":
+  main()
